795bfeb293a565308aa6651ec4a602b172e0bc0e
jmorgan <jamorgan8@gmail.com>
2022-08-23 22:09:54 -0500

    starting q5

--- script/script.sql
+++ script/script.sql
@@ -26,23 +26,39 @@ ORDER BY height;
 --Answer-- Eddie Gaedel, 43" tall, 1 appearance with St Louis Browns
 
 
--- 3. Find all players in the database who played at Vanderbilt University. Create a list showing each player’s first and last names as well as the total salary they earned in the major leagues. Sort this list in descending order by the total salary earned. Which Vanderbilt player earned the most money in the majors?
-
+/*3. Find all players in the database who played at Vanderbilt University. Create a list showing each player’s first and last names as well as the total salary they earned in the major leagues. Sort this list in descending order by the total salary earned. Which Vanderbilt player earned the most money in the majors?*/
+               
+             
 SELECT
     CONCAT(namelast, ', ', namefirst) AS name,
-    SUM(salary) AS major_league_salary
-FROM people AS p
-LEFT JOIN collegeplaying AS c
-    ON p.playerid = c.playerid
-LEFT JOIN schools AS s1
-    ON c.schoolid = s1.schoolid
+    CAST(CAST(SUM(salary) AS NUMERIC) AS MONEY) AS major_league_salary
+FROM collegeplaying AS c
+LEFT JOIN people AS p
+    ON c.playerid = p.playerid
 LEFT JOIN salaries AS s2
     ON p.playerid = s2.playerid
-WHERE schoolname = 'Vanderbilt University'
-    AND SUM(salary) IS NOT NULL
+WHERE schoolid = 'vandy'
 GROUP BY name
+HAVING SUM(salary) IS NOT NULL
 ORDER BY major_league_salary DESC;
 
+--ANSWER-- David Price, $245,553,888.00
 
 
+/*4. Using the fielding table, group players into three groups based on their position: label players with position OF as "Outfield", those with position "SS", "1B", "2B", and "3B" as "Infield", and those with position "P" or "C" as "Battery". Determine the number of putouts made by each of these three groups in 2016.*/
+
+SELECT
+    --CONCAT(namelast, ', ', namefirst) AS player_name,
+    CASE WHEN pos = 'SS' OR pos = '1B' OR pos = '2B' OR pos = '3B' THEN 'Infield'
+         WHEN pos = 'P' OR pos = 'C' THEN 'Battery'
+         ELSE 'Outfield'
+         END AS position,
+    SUM(po) AS putouts
+FROM people AS p
+LEFT JOIN fielding AS f
+    ON p.playerid = f.playerid
+GROUP BY position;
+
+--ANSWER-- Battery= 2,575,499, Infield = 6,101,378, Outfiled = 2,731,506
+   
     
